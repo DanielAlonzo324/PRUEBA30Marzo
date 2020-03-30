6b5489c37b7fd7e715b05419e8594a05029c8a5a
Daniel Alonzo Herrera <dan13lalonzo@gmail.com>
2020-03-30 17:50:26 -0600

    Segunda Modificación.

--- ClonarProyecto/src/main/java/com/mycompany/clonarproyecto/Main.java
+++ ClonarProyecto/src/main/java/com/mycompany/clonarproyecto/Main.java
@@ -6,6 +6,9 @@ public class Main {
     
     public static void main(String[] args) {
         
+        System.out.println("Daniel Alonzo Herrera ");
+        System.out.println("7a. AVE 9-81 Zona 5, Huehuetenango, Huehuetenango ");
+        System.out.println("3059-6671 ");
         
     }
     
