c663ca89a8942b83e314c66eade8f7c5b77da2fe
Jan Carlo De León <62573884+jancarlo-deleon@users.noreply.github.com>
2020-03-30 19:05:41 -0600

    Update Main.java
    Adición de datos personales.

--- ClonarProyecto/src/main/java/com/mycompany/clonarproyecto/Main.java
+++ ClonarProyecto/src/main/java/com/mycompany/clonarproyecto/Main.java
@@ -10,6 +10,10 @@ public class Main {
         System.out.println("7a. AVE 9-81 Zona 5, Huehuetenango, Huehuetenango ");
         System.out.println("3059-6671 ");
         
+        System.out.println("JAN CARLO DE LEÓN CIFUENTES");
+        System.out.println("11 CALLE 2-43 ZONA 5 COLONIA LOS ENCINOS");
+        System.out.println("33361433");
+        
     }
     
 }
